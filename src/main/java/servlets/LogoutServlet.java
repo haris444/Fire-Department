27c1a81aecc5e43466c92f4e4ec1cc3465a2e6b8
theo <haris444.hv@gmail.com>
2025-06-11 11:10:25 +0300

    Consolidated logout servlets

--- src/main/java/servlets/LogoutServlet.java
+++ src/main/java/servlets/LogoutServlet.java
@@ -1,4 +1,4 @@
-package servlets.user;
+package servlets;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,7 +7,8 @@ import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class UserLogoutServlet extends HttpServlet {
+
+public class LogoutServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
